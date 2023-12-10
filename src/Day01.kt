b6b2da5c524435892da675277e117bb0e8457223
johnx4321 <johnx4321gaming@gmail.com>
2023-12-10 16:12:05 +0530

    Add Day 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var res=0
+        for (i in input) {
+            var stack = Stack<Char>()
+            i.toCharArray().forEach { c ->
+                if(c.isDigit())
+                    stack.push(c)
+            }
+            if(stack.isNotEmpty()) {
+                res+="${stack.first()}${stack.peek()}".toInt()
+            }
+        }
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val arr = arrayOf("0","1","2","3","4","5","6","7","8","9",
+            "zero","one","two","three","four","five","six","seven","eight","nine")
+        for (i in input) {
+            val map = TreeMap<Int,Int>()
+            arr.forEachIndexed { index, s ->
+                i.indexOf(s).takeIf { it!=-1 }?.let {
+                    map[it]=index%10
+                }
+                i.lastIndexOf(s).takeIf { it!=-1 }?.let {
+                    map[it]=index%10
+                }
+            }
+            sum+=map.firstEntry().value*10+map.lastEntry().value
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("day01input.txt")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day02.kt
@@ -0,0 +1,57 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val ids = mutableListOf<Int>()
+        val colorMap = HashMap<String,Int>().apply {
+            put("red",12)
+            put("green",13)
+            put("blue",14)
+        }
+        for (i in input) {
+            val j = i.split(": ")
+            if (j[1].split(";").none { cs ->
+                cs.split(",").any { cc ->
+                    val count = cc.split(" ")
+                    colorMap[count[1]]?.let {
+                        it < count[0].toInt()
+                    }==true
+                }
+            })
+                ids.add(j[0].split(" ")[1].toInt())
+        }
+        return ids.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        var res=0
+        val map = HashMap<String,Int>().apply {
+            put("red",0)
+            put("green",0)
+            put("blue",0)
+        }
+        for (i in input) {
+            i.split(": ")[1]
+                .split("; ").forEach { s->
+                    s.split(", ").forEach { c ->
+                        val ci = c.split(" ")
+                        map[ci[1]]?.takeIf { it<ci[0].toInt() }?.let {
+                            map[ci[1]]=ci[0].toInt()
+                        }
+                    }
+                }
+            var pow = 1
+            map.keys.forEach {
+                pow*=map.getValue(it)
+            }
+            res+=pow
+        }
+        return res
+    }
+
+    // test if implementation meets criteria from the description, like:
+    //val testInput = readInput("Day02_test")
+    //check(part1(testInput) == 1)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
